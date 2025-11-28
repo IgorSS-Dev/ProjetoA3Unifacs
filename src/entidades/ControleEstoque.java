6e64725cc8e190299f373853171ebde6289a074f
Igor Sousa <igor.sousa00524@gmail.com>
2025-11-28 01:38:14 -0300

    doc: Alterei uma classe

--- src/entidades/ControleEstoque.java
+++ src/entidades/ControleEstoque.java
@@ -16,9 +16,9 @@ public class ControleEstoque {
         listaDeProdutos.add(new Hardware("Placa de Vídeo RTX 4060 OC", "Hardware", 2399.90, "Gigabyte", 15, "GV-N4060OC", 24, 115, "8GB GDDR6"));
         listaDeProdutos.add(new Hardware("SSD 1TB 980 Pro", "Hardware", 699.90, "Samsung", 30, "MZ-V8P1T0B", 8, 60, "Leitura 7000MB/s"));
         listaDeProdutos.add(new Hardware("Placa Mãe Asus TUF Gaming B550M", "Hardware", 949.90, "Asus", 12, "TUF-B550M-PLUS", 12, 50, "Socket AM4"));
-        listaDeProdutos.add(new Hardware("Fonte Corsair RM750e 750W", "Hardware", 699.90, "Corsair", 25, "CP-9020248", 60, 750, "80 Plus Gold"));
-        listaDeProdutos.add(new Hardware("HD Seagate Barracuda 2TB", "Hardware", 329.90, "Seagate", 40, "ST2000DM008", 12, 8, "7200RPM SATA III"));
-        listaDeProdutos.add(new Hardware("Memória RAM XPG Spectrix D35G 8GB", "Hardware", 169.90, "XPG", 60, "AX4U32008G16A-SBKD35G", 120, 3, "DDR4 3200MHz CL16"));
+        listaDeProdutos.add(new Periferico("Headset Gamer HyperX Cloud Stinger", "Periférico", 249.90, "HyperX", 20, "P2/P3", "Preto/Vermelho"));
+        listaDeProdutos.add(new Periferico("Webcam Logitech C920 Full HD", "Periférico", 399.90, "Logitech", 15, "USB", "Preto"));
+        listaDeProdutos.add(new Periferico("Controle Xbox Series Wireless", "Periférico", 449.90, "Microsoft", 10, "USB-C / Bluetooth", "Branco"));
     }
 
 
